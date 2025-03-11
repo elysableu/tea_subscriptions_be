c732d82c0d12fd47f5e3b63fba96cbc737b9e103
elysableu <elysabward@gmail.com>
2025-03-10 19:01:14 -0600

    Refactor db

--- db/migrate/20250310234609_create_join_table_subscription_tea.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableSubscriptionTea < ActiveRecord::Migration[7.2]
-  def change
-    create_join_table :subscriptions, :teas do |t|
-      t.index [:subscription_id, :tea_id]
-      t.index [:tea_id, :subscription_id]
-    end
-  end
-end
