e712323fb48cef0924656f633698d0a5e4b33a9d
elysableu <elysabward@gmail.com>
2025-03-10 20:30:25 -0600

    Add spec files and model tests for db relationships

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe Subscription, type: :model do
+  describe "relationships" do
+    it {should have_many :subscription_customers}
+    it {should have_many(:customers).through(:subscription_customers)}
+
+    it {should have_many :subscription_teas}
+    it {should have_many(:teas).through(:subscription_teas)}
+  end
+end
