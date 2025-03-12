46b833f420077af31b04f577d16aab6e8f531bc4
elysableu <elysabward@gmail.com>
2025-03-12 13:00:21 -0600

    Add validations to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,11 @@
 class Customer < ApplicationRecord
+  # Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :address, presence: true
+
+  # Relationships
   has_many :subscription_customers
   has_many :subscriptions, through: :subscription_customers
 end
