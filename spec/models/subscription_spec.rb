13a911bc94d0c273bcc67cb5aa581e4a22f5bca6
elysableu <elysabward@gmail.com>
2025-03-12 12:57:09 -0600

    Add validations tests to subscription_spec

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Subscription, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:title)}
+    it { should validate_presence_of(:price)}
+    it { should validate_presence_of(:status)}
+    it { should validate_presence_of(:frequency)}
+  end
+
   describe "relationships" do
     it {should have_many :subscription_customers}
     it {should have_many(:customers).through(:subscription_customers)}
