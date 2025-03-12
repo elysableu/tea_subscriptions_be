55725b1b3b40007c8d31bc8c00772a69bcf6744f
elysableu <elysabward@gmail.com>
2025-03-12 12:58:12 -0600

    Add validations tests to customer_spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Customer, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:first_name)}
+    it { should validate_presence_of(:last_name)}
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:address)}
+  end
+
   describe "relationships" do
     it {should have_many :subscription_customers}
     it {should have_many(:subscriptions).through(:subscription_customers)}
