5a6e4cd1d1dc04141ba6b11ea11006d574b3cd85
elysableu <elysabward@gmail.com>
2025-03-12 13:29:13 -0600

    Add rescue_from in appllication_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: ErrorSerializer.format_error(e.message), status: :not_found
+  end
 end
