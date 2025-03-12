1455265af79c417f7f51f18609a934af6f669da0
elysableu <elysabward@gmail.com>
2025-03-12 13:01:50 -0600

    Add validations to subscriptions model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,11 @@
 class Subscription < ApplicationRecord
+  # Validations
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :frequency, presence: true
+
+  # Relationships
   has_many :subscription_customers
   has_many :customers, through: :subscription_customers
 
