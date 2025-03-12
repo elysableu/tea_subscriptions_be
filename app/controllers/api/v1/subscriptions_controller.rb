b678edaf41b27fb62b993f04d9db67dd8ccab24e
elysableu <elysabward@gmail.com>
2025-03-12 12:55:57 -0600

    Add validations test to tea_spec

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def index
-    render json: SubscriptionSerializer.new(Subscription.all, fields: {subscription: [:title, :price, :status, :frequency]}).serializable_hash
+    render json: SubscriptionSerializer.new(Subscription.all, fields: {subscription: [:title, :price, :status, :frequency]}).serializable_hash, status: :ok
   end
 
   def show
     subscription = Subscription.find(params[:id])
-    render json: SubscriptionSerializer.new(subscription, include: [:teas, :customers]).serializable_hash
+    render json: SubscriptionSerializer.new(subscription, include: [:teas, :customers]).serializable_hash, status: :ok
   end
 
   def update
-    render json: Subscription.update(params[:id], params.require(:subscription).permit(:status))
+    render json: Subscription.update(params[:id], params.require(:subscription).permit(:status)), status: :ok
   end
 end
